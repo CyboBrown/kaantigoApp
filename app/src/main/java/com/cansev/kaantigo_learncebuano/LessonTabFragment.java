a77005edfda1317ec50378d7722f909adbb959f8
CyboBrown <seville2020christian@gmail.com>
2023-04-06 17:57:16 +0800

    Initial commit

---
+++ app/src/main/java/com/cansev/kaantigo_learncebuano/LessonTabFragment.java
@@ -0,0 +1,105 @@
+package com.cansev.kaantigo_learncebuano;
+
+import android.animation.LayoutTransition;
+import android.graphics.drawable.Icon;
+import android.os.Bundle;
+
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+
+import android.transition.AutoTransition;
+import android.transition.ChangeClipBounds;
+import android.transition.ChangeTransform;
+import android.transition.Explode;
+import android.transition.Fade;
+import android.transition.Slide;
+import android.transition.TransitionManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LessonTabFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LessonTabFragment extends Fragment implements View.OnClickListener {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public LessonTabFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LessonTabFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LessonTabFragment newInstance(String param1, String param2) {
+        LessonTabFragment fragment = new LessonTabFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    CardView card_lesson1;
+    LinearLayout cardop_lesson1;
+    ImageView expand_card;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_lesson_tab, container, false);
+        card_lesson1 = view.findViewById(R.id.card_lesson1);
+        card_lesson1.setOnClickListener(this);
+        card_lesson1.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        System.out.println("Clicked");
+        switch (view.getId()) {
+            case R.id.card_lesson1:
+                cardop_lesson1 = view.findViewById(R.id.cardop_lesson1);
+                expand_card = view.findViewById(R.id.expand_card);
+//                TransitionManager.beginDelayedTransition(card_lesson1, new AutoTransition());
+                if (cardop_lesson1.getVisibility() == View.GONE) {
+                    TransitionManager.beginDelayedTransition(card_lesson1, new AutoTransition());
+                    cardop_lesson1.setVisibility(View.VISIBLE);
+                    expand_card.setImageIcon(Icon.createWithResource(getActivity(),R.drawable.ic_expand_less));
+                } else {
+                    TransitionManager.beginDelayedTransition(card_lesson1, new ChangeClipBounds());
+                    cardop_lesson1.setVisibility(View.GONE);
+                    expand_card.setImageIcon(Icon.createWithResource(getActivity(),R.drawable.ic_expand_more));
+                }
+        }
+    }
+}
---
+++ app/src/main/java/com/cansev/kaantigo_learncebuano/MainActivity.java
@@ -0,0 +1,56 @@
+package com.cansev.kaantigo_learncebuano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    LessonTabFragment lessonTabFragment = new LessonTabFragment();
+    ToolsTabFragment toolsTabFragment = new ToolsTabFragment();
+    SearchTabFragment searchTabFragment = new SearchTabFragment();
+    QuizTabFragment quizFragment = new QuizTabFragment();
+    ProfileTabFragment profileFragment = new ProfileTabFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.page_1);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, lessonTabFragment).commit();
+        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.page_4);
+        badgeDrawable.setNumber(69);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()) {
+                case R.id.page_1:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, lessonTabFragment).commit();
+                    System.out.println("Home");
+                    return true;
+                case R.id.page_2:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, toolsTabFragment).commit();
+                    System.out.println("Tools");
+                    return true;
+                case R.id.page_3:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, searchTabFragment).commit();
+                    System.out.println("Search");
+                    return true;
+                case R.id.page_4:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, quizFragment).commit();
+                    System.out.println("Quiz");
+                    return true;
+                case R.id.page_5:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                    System.out.println("Profile");
+                    return true;
+            }
+            return false;
+        });
+    }
+}
